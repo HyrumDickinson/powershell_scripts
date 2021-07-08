77abe799654279030eec97891a9dab678909bf09
Hyrum Dickinson <hyrumsd2@illinois.edu>
2021-07-08 18:39:20 -0500

    it's identical to finish.ps1 lmao

---
+++ start.ps1
@@ -0,0 +1,16 @@
+git pull
+git push
+Set-Location ..
+Set-Location Solar_Panel_Protection_System
+git pull
+git push
+Set-Location ..
+Set-Location Connect_Four_AI
+git pull
+git push
+Set-Location ..
+Set-Location TicTacToe_AI
+git pull
+git push 
+Set-Location ..
+Set-Location powershell_scripts
