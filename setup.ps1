8ec705c85e0c1056bbde76256241bb5c6b6d18b9
Hyrum Dickinson <hyrumsd2@illinois.edu>
2021-07-08 20:17:28 -0500

    sets up coding folder ready to go

---
+++ setup.ps1
@@ -0,0 +1,7 @@
+mkdir Code 
+Set-Location Code 
+git clone https://github.com/HyrumDickinson/Connect_Four_AI
+git clone https://github.com/HyrumDickinson/TicTacToe_AI
+git clone https://github.com/HyrumDickinson/Solar_Panel_Protection_System
+git clone https://github.com/HyrumDickinson/powershell_scripts
+
