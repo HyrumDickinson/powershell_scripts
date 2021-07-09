8f31cc5dd1dff6efe828b5f72ac6081efd58415e
Hyrum Dickinson <hyrumsd2@illinois.edu>
2021-07-08 20:09:10 -0500

    updates conda & wsl

---
+++ update.ps1
@@ -0,0 +1,12 @@
+conda update anaconda
+conda update conda
+conda update python
+conda update -n python2 --all
+conda update -n python3 --all
+conda update -n python3_tictactoe --all
+conda update -n python3_connectfour --all
+
+Update-Module
+
+# wsl.exe sudo apt-get upgrade
+# wsl.exe sudo apt-get update
