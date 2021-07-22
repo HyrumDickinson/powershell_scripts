05296212c604ca0ade314b2ccad93cada1e06d40
Hyrum Dickinson <hyrumsd2@illinois.edu>
2021-07-22 14:43:21 -0500

    removed comments

--- setup.ps1
+++ setup.ps1
@@ -4,10 +4,10 @@ git clone https://github.com/HyrumDickinson/TicTacToe_AI
 git clone https://github.com/HyrumDickinson/Solar_Panel_Protection_System
 git clone https://github.com/HyrumDickinson/powershell_scripts
 
-# conda create --name python2_solarpanels python=2
-# conda activate python2_solarpanels
-# conda install pillow
-# conda install matplotlib
-# conda deactivate
-# conda create --name python3_tictactoe
-# conda create --name python3_connectfour
+conda create --name python2_solarpanels python=2
+conda activate python2_solarpanels
+conda install pillow
+conda install matplotlib
+conda deactivate
+conda create --name python3_tictactoe
+conda create --name python3_connectfour
