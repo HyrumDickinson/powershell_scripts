b219de81098769056fddcbc2b4fe9b8d1085851c
Hyrum Dickinson <hyrumsd2@illinois.edu>
2021-07-22 14:42:11 -0500

    changed file location

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,4 @@
-mkdir Code 
-Set-Location Code 
+Set-Location ..
 git clone https://github.com/HyrumDickinson/Connect_Four_AI
 git clone https://github.com/HyrumDickinson/TicTacToe_AI
 git clone https://github.com/HyrumDickinson/Solar_Panel_Protection_System
